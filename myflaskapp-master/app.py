23be0d2f131ee89f378d9c7d1e3af5fece114ac4
vikvo11 <woroncull@yandex.ru>
2020-04-04 18:09:48 +0300

    upd

--- myflaskapp-master/app.py
+++ myflaskapp-master/app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-#from data import Articles
-#from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
@@ -9,23 +7,10 @@ from flask import g
 
 app = Flask(__name__)
 
-# Config MySQL
-#app.config['MYSQL_HOST'] = 'localhost'
-#app.config['MYSQL_USER'] = 'root'
-#app.config['MYSQL_PASSWORD'] = '123456'
-#app.config['MYSQL_DB'] = 'myflaskapp'
-#app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-
+# Config DB
 #app.config['DATABASE']='Dev.db'
 app.config['DATABASE']='Test.db'
 
-
-#DATABASE = 'Dev.db'
-
-# init MYSQL
-#mysql = MySQL(app)
-
-#Articles = Articles()
 
 def get_db():
 	db = getattr(g, '_database', None)
@@ -73,12 +58,7 @@ db.row_factory = make_dicts
 '''
 
 def valid_login(username, password):
-    #user = query_db('select * from users where name = ? and password = ?', [username, password], one=True)
     user = query_db('select * from users where username = ?',[username], one=True)
-    #if sha256_crypt.verify(password,user['password']):
-    #    print("TRUE")
-    #else:
-    #    print("FALSE")
     if user is None:
         print(username +" is NONE")
         return False
@@ -92,10 +72,7 @@ def log_the_user_in(username):
 
 def register_user(name, email, username, password):
     query_db("INSERT INTO users(name, email, username, password) VALUES(?,?,?,?)",(name, email, username, password))
-    #cur = get_db().cursor()
-    #cur.execute("INSERT INTO users(name, email, username, password) VALUES(?,?,?,?)",(name, email, username, password))
     get_db().commit()
-    #cur.close()
 			 
 #import form menu.py >>
 #cur.execute("CREATE TABLE IF NOT EXISTS users(name text, email string, username string, password string)")
@@ -116,13 +93,6 @@ def about():
 # Articles
 @app.route('/articles')
 def articles():
-    # Create cursor
-    #cur = mysql.connection.cursor()
-    #con = sqlite3.connect(DATABASE)
-    #con.row_factory = dict_factory
-    #cur=con.cursor()
-	#with app.app_context():
-	#cur = get_db().cursor()
 	#Easy Querying
 	for title in query_db('select * from articles_v'):
 		print (title['title'], 'has the id', title['id'])
@@ -137,21 +107,10 @@ def articles():
 	# Get articles
 	try:
 		articles =query_db("SELECT * FROM articles_v")
-		#result = cur.execute("SELECT * FROM articles_v")
-		#articles = cur.fetchall()
-		#for article in articles:
-			#print(article['title'])
-			#print(article)
-		#print(articles)
 	except:
-		#cur.close()
-		#con.close()
 		msg = 'No Articles Found'
 		return render_template('articles.html', msg=msg)
 	finally:
-	# Close connection
-		#cur.close()
-		#con.close()
 		if articles:
 			return render_template('articles.html', articles=articles)
 		else:
@@ -160,26 +119,12 @@ def articles():
 #Single Article
 @app.route('/article/<string:id>/')
 def article(id):
-    # Create cursor
-    #cur = mysql.connection.cursor()
-    #con = sqlite3.connect(DATABASE)
-    #con.row_factory = dict_factory
-    #cur=con.cursor()
-	#with app.app_context():
-	#cur = get_db().cursor()
-	# Get article
 	try:
 		article =query_db("SELECT * FROM articles_v WHERE id = ?",[id],one=True)
-		#result = cur.execute("SELECT * FROM articles_v WHERE id = ?", [id])
-		#article = cur.fetchone()
 	except:
-		#cur.close()
-		#con.close()
 		msg = 'No Articles Found'
 		return render_template('articles.html', msg=msg)
 	finally:
-		#cur.close()
-		#con.close()
 		return render_template('article.html', article=article)
 
 ## Register Form Class
@@ -206,7 +151,7 @@ class TemplateForm(Form):
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     form = RegisterForm(request.form)
-    if request.method == 'POST' and form.validate():
+    if request.method == 'POST':#and form.validate():
         password = sha256_crypt.encrypt(str(form.password.data))
         print('test')
         register_user(request.form['name'], request.form['email'], request.form['username'],password)
@@ -257,27 +202,12 @@ def logout():
 @app.route('/dashboard')
 @is_logged_in
 def dashboard():
-	#with app.app_context():
-	#cur = get_db().cursor()
-	# Create cursor
-	#cur = mysql.connection.cursor()
-	#con = sqlite3.connect(DATABASE)
-	#con.row_factory = dict_factory
-	#cur=con.cursor()
-	# Get articles
 	try:
-		#result = cur.execute("SELECT * FROM articles_v")
-		#articles = cur.fetchall()
 		articles =query_db("SELECT * FROM articles_v")
 	except:
-		#cur.close()
-		#con.close()
 		msg = 'No Articles Found'
 		return render_template('dashboard.html', msg=msg)
 	finally:
-	# Close connection
-		#cur.close()
-		#con.close()
 		if articles:
 			return render_template('dashboard.html', articles=articles)
 		else:
@@ -293,31 +223,12 @@ def add_article():
 	if request.method == 'POST' and form.validate():
 		title = form.title.data
 		body = form.body.data
-	
-		# Create Cursor
-		#cur = mysql.connection.cursor()
-		#con = sqlite3.connect(DATABASE)
-		#con.row_factory = dict_factory
-		#cur=con.cursor()
-		#cur = get_db().cursor()
-		# Execute
 		try:
-			#cur.execute("CREATE TABLE IF NOT EXISTS articles_v(id INTEGER PRIMARY KEY,title text, body string, author string)")
-			#cur.execute("INSERT INTO articles(title, body, author) VALUES(%s, %s, %s)",(title, body, session['username']))
-			#cur.execute("INSERT INTO articles_v(title, body, author) VALUES(?, ?, ?)",(title, body, session['username']))
 			query_db("INSERT INTO articles_v(title, body, author) VALUES(?, ?, ?)",(title, body, session['username']))
-	
-			# Commit to DB
-			#mysql.connection.commit()
 			get_db().commit()
-			#Close connection
-			#cur.close()
-			#con.close()
 			flash('Article Created', 'success')
 		except:
-			#cur.close()
 			app.logger.info('CREATE ERROR')
-			#cur.close()
 			error = 'CREATE ERROR'
 			return render_template('home.html', error=error)	
 	
@@ -330,23 +241,9 @@ def add_article():
 @is_logged_in
 def edit_(id):
 	#with app.app_context():
-	print('START...')
-	# Create cursor
-	#cur = mysql.connection.cursor()
-	#con = sqlite3.connect(DATABASE)
-	#con.row_factory = dict_factory
-	#cur=con.cursor()
-	#cur = get_db().cursor()
-	# Get article by id
 	try:
-		print('START TRY...')
-		#result = cur.execute("SELECT * FROM template WHERE id = ?", [id])
-		#article = cur.fetchone()
 		article=query_db("SELECT * FROM template WHERE id = ?", [id],one=True)
-		#cur.close()
 		print(article['name'])
-		#con.close()
-		# Get form
 		form = TemplateForm(request.form)
 	
 		# Populate article form fields
@@ -356,7 +253,6 @@ def edit_(id):
 		form.link.data = article['link']
 	except:
 		print('Error')
-		#cur.close()
 		error = 'No Template Found'
 		return render_template('home.html', error=error)
 	if request.method == 'POST' and form.validate():
@@ -364,33 +260,16 @@ def edit_(id):
 		body = request.form['body']
 		param = request.form['param']
 		link = request.form['link']
-	
-		# Create Cursor
-		#cur = mysql.connection.cursor()
-		#con = sqlite3.connect(DATABASE)
-		#con.row_factory = dict_factory
-		#cur=con.cursor()
-		#cur = get_db().cursor()
 		app.logger.info(name)
 		try:
-			# Execute
-			#cur.execute ("UPDATE template SET name=?, body=?, param=?, link=? WHERE id=?",(name, body,param,link, id))
 			query_db("UPDATE template SET name=?, body=?, param=?, link=? WHERE id=?",(name, body,param,link, id))
-			# Commit to DB
-			#mysql.connection.commit()
 			get_db().commit()
-			#Close connection
-			#cur.close()
 		except:
 			app.logger.info('UPDATE ERROR')
-			#cur.close()
 			error = 'UPDATE ERROR'
 			return render_template('home.html', error=error)				
-		#con.close()
-		#cur.close()
 		flash('Template Updated', 'success')
 		return redirect(url_for('index'))
-	#cur.close()
 	return render_template('edit_.html', form=form)
 
 # Edit Article
@@ -398,20 +277,8 @@ def edit_(id):
 @is_logged_in
 def edit_article(id):
 	#with app.app_context():
-	# Create cursor
-	#cur = mysql.connection.cursor()
-	#con = sqlite3.connect(DATABASE)
-	#con.row_factory = dict_factory
-	#cur=con.cursor()
-	#cur = get_db().cursor()
-	# Get article by id
 	try:
-		#result = cur.execute("SELECT * FROM articles_v WHERE id = ?", [id])
-		#article = cur.fetchone()
 		article =query_db("SELECT * FROM articles_v WHERE id = ?", [id],one=True)
-		#cur.close()
-		#con.close()
-		# Get form
 		form = ArticleForm(request.form)
 		
 		# Populate article form fields
@@ -419,7 +286,6 @@ def edit_article(id):
 		form.body.data = article['body']
 	except:
 		print('Error')
-		#cur.close()
 		error = 'No Article Found'
 		return render_template('home.html', error=error)		
 	
@@ -428,34 +294,18 @@ def edit_article(id):
 		title = request.form['title']
 		body = request.form['body']
 	
-		# Create Cursor
-		#cur = mysql.connection.cursor()
-		#con = sqlite3.connect(DATABASE)
-		#con.row_factory = dict_factory
-		#cur=con.cursor()
-		#cur = get_db().cursor()
-	
 		app.logger.info(title)
 		try:
 			# Execute
-			#cur.execute ("UPDATE articles_v SET title=?, body=? WHERE id=?",(title, body, id))
 			query_db("UPDATE articles_v SET title=?, body=? WHERE id=?",(title, body, id))
-			# Commit to DB
-			#mysql.connection.commit()
 			get_db().commit()
-			#Close connection
-			#cur.close()
 		except:
 			app.logger.info('UPDATE ERROR')
-			#cur.close()
 			error = 'UPDATE ERROR'
 			return render_template('home.html', error=error)				
-		#con.close()
 	
 		flash('Article Updated', 'success')
-		#cur.close()
 		return redirect(url_for('dashboard'))
-	#cur.close()
 	return render_template('edit_article.html', form=form)
 
 # Delete Article
@@ -463,26 +313,13 @@ def edit_article(id):
 @is_logged_in
 def delete_article(id):
 	#with app.app_context():
-	# Create cursor
-	#cur = mysql.connection.cursor()
-	#con = sqlite3.connect(DATABASE)
-	#con.row_factory = dict_factory
-	#cur=con.cursor()
-	#cur = get_db().cursor()
 	try:
 		# Execute
-		#cur.execute("DELETE FROM articles_v WHERE id = ?", [id])
 		query_db("DELETE FROM articles_v WHERE id = ?", [id])
-		# Commit to DB
-		#mysql.connection.commit()
 		get_db().commit()
-		#Close connection
-		#cur.close()
-		#con.close()
 		flash('Article Deleted', 'success')
 	except:
 		app.logger.info('Delete ERROR')
-		#cur.close()
 		error = 'DELETE ERROR'
 		return render_template('home.html', error=error)			
 	
--- myflaskapp-master/test_basic.py
+++ myflaskapp-master/test_basic.py
@@ -2,14 +2,23 @@ import os
 import unittest
 #from flask import Flask
 #from flask import session
-
-from app import app
+import sqlite3
+from app import app,query_db
+from flask import g
+import random
+import string
 #app = Flask(__name__)
 
 #from blog.views import app as app1
 #app.register_blueprint(app1)
 
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+#print ("Random String is ", randomString(10) )
 TEST_DB = 'myblog_test.sqlite'
+
 
 
 class BasicTests(unittest.TestCase):
@@ -53,7 +62,36 @@ class BasicTests(unittest.TestCase):
         response = self.app.get('/edit_/1', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         print(response.data)
-
+		
+    def test_register_login(self):
+        #with app.app_context():
+            #for user in query_db('select * from users'):
+               # print(user['name'])
+        with app.test_client() as c:
+            #c.DATABASE = 'Test.db'
+            app.config['DATABASE']='Test.db'
+            #c.config['DATABASE']='Test.db'
+            #with c.session_transaction() as session:
+                #session['logged_in'] = True
+                #session['username'] = 'Test'
+            # send data as POST form to endpoint
+            
+            for x in range(5):
+                name=randomString(10)
+                passw=randomString(10)
+                sent = {'name':'test'+str(x),'email':'email@email'+str(x),'username':name,'password':passw}
+                print(sent)
+                result = c.post('/register',data=sent)
+                sent = {'username':name,'password':passw}
+                request = c.post('/login',data=sent)
+                print(request.data)
+            # check result from server with expected data
+            #self.assertEqual(
+            #    result.data,
+            #    json.dumps(sent)
+            #)
+            #print(result.data)
+            #self.assertEqual(result.status_code, 200)
     
     def test_main(self):
         with app.test_client() as client:
@@ -72,6 +110,9 @@ class BasicTests(unittest.TestCase):
             #self.assertEqual(result.status_code, 200)
 	
     def test_add_article(self):
+        with app.app_context():
+            for user in query_db('select * from users'):
+                print(user['name'])
         with app.test_client() as c:
             #c.DATABASE = 'Test.db'
             app.config['DATABASE']='Test.db'
@@ -80,11 +121,10 @@ class BasicTests(unittest.TestCase):
                 session['logged_in'] = True
                 session['username'] = 'Test'
             # send data as POST form to endpoint
-            sent = {'title':'test','body':'testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest'}
-            result = c.post(
-                '/add_article',
-                data=sent
-            )
+            
+            for x in range(5):
+                sent = {'title':'test'+str(x),'body':'testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest'}
+                result = c.post('/add_article',data=sent)
             # check result from server with expected data
             #self.assertEqual(
             #    result.data,
@@ -92,8 +132,85 @@ class BasicTests(unittest.TestCase):
             #)
             print(result.data)
             #self.assertEqual(result.status_code, 200)
+			
+    def test_edit_article(self):
+        with app.app_context():
+            articles=query_db('select * from articles_v')
+            #for article in articles:
+                #print(article['id'])
+        with app.test_client() as c:
+            #c.DATABASE = 'Test.db'
+            app.config['DATABASE']='Test.db'
+            #c.config['DATABASE']='Test.db'
+            with c.session_transaction() as session:
+                session['logged_in'] = True
+                session['username'] = 'Test'
+            # send data as POST form to endpoint
+            for article in articles:
+                print(article['id'],article['body'])
+                sent = {'title':'EDIT'+str(article['id']),'body':str(article['body'])+'-EDIT'}
+                result = c.post('/edit_article/'+str(article['id']),data=sent)
+            # check result from server with expected data
+            #self.assertEqual(
+            #    result.data,
+            #    json.dumps(sent)
+            #)
+            #print(result.data)
+            #self.assertEqual(result.status_code, 200)
+			
+    def test_open_article(self):
+        with app.app_context():
+            articles=query_db('select * from articles_v')
+            #for article in articles:
+                #print(article['id'])
+        with app.test_client() as c:
+            #c.DATABASE = 'Test.db'
+            app.config['DATABASE']='Test.db'
+            #c.config['DATABASE']='Test.db'
+            with c.session_transaction() as session:
+                session['logged_in'] = True
+                session['username'] = 'Test'
+            # send data as POST form to endpoint
+            for article in articles:
+                print(article['id'])
+                print('/article/'+str(article['id']))
+                #sent = {'title':'EDIT'+str(article['id']),'body':str(article['body'])+'-EDIT'}
+                #result = c.post('/edit_article/'+str(article['id']),data=sent)
+                response = self.app.get('/article/'+str(article['id']), follow_redirects=True)
+                print(response.data)
+                self.assertEqual(response.status_code, 200)
+            # check result from server with expected data
+            #self.assertEqual(
+            #    result.data,
+            #    json.dumps(sent)
+            #)
+            #print(result.data)
+            #self.assertEqual(result.status_code, 200)
 
-
+    def test_delete_article(self):
+        with app.app_context():
+            articles=query_db('select * from articles_v')
+            #for article in articles:
+                #print(article['id'])
+        with app.test_client() as c:
+            #c.DATABASE = 'Test.db'
+            app.config['DATABASE']='Test.db'
+            #c.config['DATABASE']='Test.db'
+            with c.session_transaction() as session:
+                session['logged_in'] = True
+                session['username'] = 'Test'
+            # send data as POST form to endpoint
+            for article in articles:
+                print(article['id'])
+                #sent = {'title':'EDIT'+str(article['id']),'body':str(article['body'])+'-EDIT'}
+                result = c.post('/delete_article/'+str(article['id']))
+            # check result from server with expected data
+            #self.assertEqual(
+            #    result.data,
+            #    json.dumps(sent)
+            #)
+            print(result.data)
+            #self.assertEqual(result.status_code, 200)
 if __name__ == "__main__":
     unittest.main()
 
