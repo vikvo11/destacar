834422e6b760f8d23727664473a5bbd1b7c5288e
vikvo11 <woroncull@yandex.ru>
2020-04-10 13:07:52 +0300

    update

--- flask-blueprint/blue/__init__.py
+++ flask-blueprint/blue/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
-
+import os
+import os.path as op
 app = Flask(__name__)
 
 
@@ -14,12 +15,18 @@ app.config['DATABASE']='blue/site/Test.db'
 #app.config['DATABASE']='blue/site/Dev.db'
 app.secret_key='secret123'
 
+#app_dir = op.realpath(os.path.dirname(__file__))
+#file_path = op.join(app_dir, 'static','files')
+#app.config['PATH_FILES']=file_path
+#print('YRAAAAAAAAAAAAAAAAAAAAAA')
+#print(app.config['PATH_FILES'])
+
 app.register_blueprint(site.routes.mod)
 app.register_blueprint(api.routes.mod, url_prefix='/api')
 app.register_blueprint(admin.routes.mod, url_prefix='/admin')
 
 
-
+#root=sys.modules['__main__'].__file__
 
 ##Для быстрого открытия ресурсов из этого каталога можно воспользоваться функцией open_resource():
 '''
@@ -31,4 +38,4 @@ with admin.routes.mod.open_resource('static/css/shop-homepage.css') as f:
     code = f.read()
 print (code)
 
-test()
+#test()
--- flask-blueprint/blue/config.py
+++ flask-blueprint/blue/config.py
@@ -1,3 +1,6 @@
-#import os
-print ('Loading the modules in the blue\site folder...')
-mod.config['DATABASE']='Dev.db'
+import os
+#print ('Loading the modules in the blue\site folder...')
+#mod.config['DATABASE']='Dev.db'
+ROOT_DIR=os.path.dirname(os.path.abspath(__file__))
+file_path=os.path.join(ROOT_DIR,'static','files')
+#database_path = os.path.join(ROOT_DIR, app.config['DATABASE_FILE'])
